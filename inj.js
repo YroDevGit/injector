9e7e51420d1adfe853d49deaa2cfb990b7190638
Tyrone <tyrone@clearmindai.com>
2025-08-03 21:02:59 +0800

    load

--- inj.js
+++ inj.js
@@ -1,16 +1,1 @@
-/**
- * 
- window.addEventListener("DOMContentLoaded", function () {
-   mypost({
-        url: `${apiURL}/special`,
-        method: "GET",
-        success: function(response){
-            console.log(response);
-        },
-        error: function(error){
-            console.log(error);
-        }
-   });
-});
- * 
- */
+console.log("Main page loaded");
