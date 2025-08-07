7ddfe38d6e05816d996db46d53f1ff6dc0bfcdbf
Tyrone <tyrone@clearmindai.com>
2025-08-07 14:39:18 +0800

    okay lets do it

--- special.js
+++ special.js
@@ -1,1 +1,12 @@
-console.log("SUCCESS");
+ window.addEventListener("DOMContentLoaded", function () {
+   mypost({
+        url: `${apiURL}/special`,
+        method: "GET",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(error){
+            console.log(error);
+        }
+   });
+});
